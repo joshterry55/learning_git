c4bfeb1d6ed3d23625e595e071ebdfc45d8614f2
Josh Terry <joshterry55@gmail.com>
2016-10-26 11:14:30 -0600

    age case statement

--- test.rb
+++ test.rb
@@ -11,3 +11,16 @@ if name == 'dave' && age == 35
 else
   puts "Hello, #{name} you are #{age} years old."
 end
+
+# Feature Branch Code
+
+case age
+when 18
+  puts "you can now smoke cigs!"
+when 21
+  puts "yay, drinking and gambling!"
+when 26
+  puts "this is the last milestone, lets rent a car!"
+else
+  "Cool, youre an age."
+end
